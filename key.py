25c587737121173cdfed356f3a7d3253c6fc1c62
paulsbrookes <paul.s.brookes@gmail.com>
2015-12-27 15:54:12 +0000

    Flesh our Key and Message classes.

--- key.py
+++ key.py
@@ -1,17 +1,28 @@
 import numpy as np
+import random
 
 class Key(object):
-    def __init__(self,key):
-        Key.key = key
+    def __init__(self, key=[]):
+        self.key = key
 
-    def substitute(self,mapping):
-        new_key = np.zeros(Key.key.shape[0])
-        for x in range(Key.key.shape[0]):
-            new_key[x] = mapping[Key.key[x]]
+    def substitute(self, mapping):
+        new_key = np.zeros(self.key.shape[0])
+        for x in range(self.key.shape[0]):
+            new_key[x] = mapping[self.key[x]]
         return new_key
 
     def invert(self):
-        inverted_key = np.zeros(Key.key.shape[0])
-        for x in range(Key.key.shape[0]):
-            inverted_key[Key.key[x]] = x
+        inverted_key = np.zeros(self.key.shape[0])
+        for x in range(self.key.shape[0]):
+            inverted_key[self.key[x]] = x
         return inverted_key
+
+    def obtain_key(self, alpha, beta):
+        key = np.zeros(len(beta))
+        for x in range(len(beta)):
+            key[x] = alpha.find(beta[x])
+        return key
+
+    def random_key(self, alpha):
+        self.key = np.array(range(len(alpha)))
+        random.shuffle(self.key)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from key import Key
+from message import Message
 import numpy as np
 
 original = np.array([1,2,0])
@@ -10,3 +11,13 @@ inverted_key = key.invert()
 
 assert np.all(new_key == np.array([2,1,0]))
 assert np.all(inverted_key == np.array([2,0,1]))
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+beta = 'qwertyuiopasdfghjklzxcvbnm'
+key = np.array(range(26))
+key[0] = 25
+key[25] = 0
+
+message = Message(alpha)
+assert message.map(key) == 'zbcdefghijklmnopqrstuvwxya'
+assert np.all(message.frequencies() == [1.0/26 for x in range(26)])
